6667358e4048286718b0bb07bdf89ace8a9b435e
wonglok <yellowhappy831@gmail.com>
2020-05-27 15:20:07 +0800

    git

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  // purge: [
+  //   './src/**/*.js',
+  //   './src/**/*.html',
+  //   './src/**/*.vue',
+  //   './src/**/*.jsx',
+  // ],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
