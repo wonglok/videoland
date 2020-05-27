b04b7ed530b2db08df5f1fa01eee5f6c0135fb4a
wonglok <yellowhappy831@gmail.com>
2020-05-27 17:10:41 +0800

    git

--- pages/poster/[id].js
+++ pages/poster/[id].js
@@ -1,10 +1,34 @@
 import { useRouter } from 'next/router'
 
-const Post = () => {
+// export async function getServerSideProps() {
+//   // Fetch data from external API
+//   const res = await fetch(`https://.../data`)
+//   const data = await res.json()
+
+//   // Pass data to the page via props
+//   return { props: { data } }
+// }
+
+export async function getServerSideProps({ req }) {
+  let account = false
+  let host = req.headers.host
+  if (host.indexOf('localhost:') === 0) {
+    account = 'wonglok831'
+  } else if (host.indexOf('.our.togethertime.me') !== -1) {
+    console.log(host)
+  }
+  return {
+    props: {
+      account,
+      host
+    }
+  }
+}
+
+const Post = ({ host, account }) => {
   const router = useRouter()
   const { id } = router.query
-
-  return <p>Post: {id}</p>
+  return <p>Post: {JSON.stringify(id)} {host} {account}</p>
 }
 
 export default Post
